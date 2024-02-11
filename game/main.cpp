c8510097923d74efe928b93848b54c7d3ca9ae2f
nicksimache <47799390+nicksimache@users.noreply.github.com>
2024-02-11 13:49:33 -0800

    Add files via upload
    Added a counter so that packets are not sent every iteration of the game loop to reduce latency, also added tilemap

--- game/main.cpp
+++ game/main.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <map>
 #include <conio.h>
+#include <fstream>
+#include <cctype>
 
 #include "Animation.h"
 
@@ -38,13 +40,16 @@ int main()
 		socket.connect(ip, 2000);
 	}
 
-	sf::RenderWindow window(sf::VideoMode(800, 800), "game", sf::Style::Default); //change window size
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "game", sf::Style::Default); //change window size
 
-	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f)); // replace with sprite
-	sf::RectangleShape player2(sf::Vector2f(100.0f, 100.0f));
+	sf::RectangleShape player(sf::Vector2f(50.0f, 50.0f));
+	sf::RectangleShape player2(sf::Vector2f(50.0f, 50.0f));
 
 	player.setOrigin(sf::Vector2f(50.0f, 50.0f)); //origin of the object
 	player2.setOrigin(sf::Vector2f(50.0f, 50.0f)); //origin of the object
+
+	player.setPosition(960, 300);
+	player2.setPosition(960, 300);
 
 	sf::Texture playerTexture;
 	sf::Texture player2Texture;
@@ -57,6 +62,57 @@ int main()
 	Animation animation2(&player2Texture, sf::Vector2u(2, 2), 0.3f);
 
 
+	//tilemap
+
+
+	std::ifstream openfile("Map1.txt");
+
+	sf::Texture brickTexture;
+	sf::Sprite brickTiles;
+	brickTexture.loadFromFile("sprites/Brick.png");
+	brickTiles.setTexture(brickTexture);
+
+	sf::Texture floorTexture;
+	sf::Sprite floorTiles;
+	floorTexture.loadFromFile("sprites/Floor.png");
+	floorTiles.setTexture(floorTexture);
+
+	int map[100][100];
+	sf::Vector2i loadCounter = sf::Vector2i(0, 0);
+
+	if (openfile.is_open())
+	{
+		while (!openfile.eof())
+		{
+			std::string str;
+			openfile >> str;
+			char x = str[0];
+			if (!isdigit(x))
+			{
+				map[loadCounter.x][loadCounter.y] = -1;
+			}
+			else
+			{
+				map[loadCounter.x][loadCounter.y] = (x - '0');
+			}
+
+			if (openfile.peek() == '\n')
+			{
+				loadCounter.x = 0;
+				loadCounter.y++;
+			}
+			else
+			{
+				loadCounter.x++;
+			}
+
+		}
+
+		loadCounter.y++;
+	}
+
+
+
 	float deltaTime = 0.0f;
 	sf::Clock clock;
 
@@ -64,7 +120,9 @@ int main()
 
 	socket.setBlocking(false);
 
-	bool update = false;
+	bool focused = false;
+
+	int socketCounter = 0;
 
 	while (window.isOpen())
 	{
@@ -79,17 +137,17 @@ int main()
 			}
 			else if (evnt.type == sf::Event::GainedFocus)
 			{
-				update = true;
+				focused = true;
 			}
 			else if (evnt.type == sf::Event::LostFocus)
 			{
-				update = false;
+				focused = false;
 			}
 		}
 
 		prevPosition = player.getPosition();
 
-		if (update)
+		if (focused)
 		{
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) // LEFT
 			{
@@ -158,14 +216,37 @@ int main()
 
 		}
 
+		if (player.getPosition().x < 352)
+		{
+			player.setPosition(352, player.getPosition().y);
+		}
+		if (player.getPosition().x > 1600)
+		{
+			player.setPosition(1600, player.getPosition().y);
+		}
+		if (player.getPosition().y < 96)
+		{
+			player.setPosition(player.getPosition().x, 96);
+		}
+		if (player.getPosition().y > 512)
+		{
+			player.setPosition(player.getPosition().x, 512);
+		}
+
 		sf::Packet packet;
 
 		if (prevPosition != player.getPosition())
 		{
 			packet << player.getPosition().x << player.getPosition().y;
-			socket.send(packet);
+			socketCounter++;
+			if (socketCounter == 3) {
+				socketCounter = 0;
+				socket.send(packet);
+			}
+	
 		}
 
+		
 		socket.receive(packet);
 		if (packet >> p2Position.x >> p2Position.y)
 		{
@@ -200,6 +281,24 @@ int main()
 		}
 		*/
 
+		for (int i = 0; i < loadCounter.x; i++) {
+			for (int j = 0; j < loadCounter.y; j++) {
+				if (map[i][j] == 1)
+				{
+					floorTiles.setPosition(i * 32, j * 32);
+					floorTiles.setTexture(floorTexture);
+					window.draw(floorTiles);
+				}
+				else if (map[i][j] == 2)
+				{
+					brickTiles.setPosition(i * 32, j * 32);
+					brickTiles.setTexture(brickTexture);
+					window.draw(brickTiles);
+				}
+			}
+		}
+
+
 		player.setTextureRect(animation.uvRect);
 		player2.setTextureRect(animation2.uvRect);
 
@@ -207,7 +306,7 @@ int main()
 		window.draw(player);
 		window.draw(player2);
 		window.display();
-		window.clear(sf::Color::White);
+		window.clear();
 	}
 
 	system("pause");
